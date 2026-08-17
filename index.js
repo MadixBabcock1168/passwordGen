60ea848725a1a8f6a8160c441b6bc46cdd14854e
Madix Babcock <madbabcock@gmail.com>
2026-08-17 00:47:04 -0500

    Added code

---
+++ index.js
@@ -0,0 +1,19 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+function getPasswords() {
+    let password1El = document.getElementById("password1-el")
+    let password2El = document.getElementById("password2-el")
+    let password1 = ""
+    for (let i = 0; i < 15; i++) {
+        let randomCharacter = characters[Math.floor(Math.random() * characters.length)]
+        password1 += randomCharacter
+        }
+        let password2 = ""
+    for (let i = 0; i < 15; i++) {
+        let randomCharacter = characters[Math.floor(Math.random() * characters.length)]
+        password2 += randomCharacter
+        }
+        password1El.textContent = password1
+        password2El.textContent = password2
+}
